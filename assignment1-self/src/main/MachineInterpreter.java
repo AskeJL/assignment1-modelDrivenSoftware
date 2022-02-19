ad68055377bbccf3cee0a4dbff8990f0dcfd8452
Aske <aske.lorenz@gm,ail.com>
2022-02-19 19:23:44 +0100

    add project

---
+++ assignment1-self/src/main/MachineInterpreter.java
@@ -0,0 +1,30 @@
+package main;
+
+import main.metamodel.Machine;
+import main.metamodel.State;
+
+public class MachineInterpreter {
+	private Machine machine; // metamodel (i.e., state machine program) to execute
+	private State currentState; // runtime state
+
+	public void run(Machine m) {
+		// TODO Auto-generated method stub sldkjngsdngljkngdgdlksg
+		
+	}
+
+	public State getCurrentState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void processEvent(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getInteger(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ assignment1-self/src/main/metamodel/Machine.java
@@ -0,0 +1,41 @@
+package main.metamodel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Machine {
+
+	private List<State> states = new ArrayList<State>();
+	private State initialState;
+	public Machine(Collection<State> states, State initialState) {
+		super();
+		this.states.addAll(states);
+		this.initialState = initialState;
+	}
+
+	public List<State> getStates() {
+
+		return states;
+	}
+
+	public State getInitialState() {
+		return initialState;
+	}
+
+	public State getState(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int numberOfIntegers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean hasInteger(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
